de3d0fa3adcc5d85c5f52036ef3c06d70e1cbc5b
prxans <pierricsans@gmail.com>
2023-12-16 13:42:44 -0500

    Validate button hides selectors and button when level is ready for play

--- frontend/main.ts
+++ frontend/main.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import { Level, Move, MoveDirection } from './protos/level_pb.js';
 
+var LEVEL: Level = new Level();
+
 class Option {
     text: string;
     move: Move;
     option: HTMLElement = document.createElement("p");
     selectedOption: HTMLElement = document.createElement("p");
     optionContainer: SelectorElement;
-
+    
     constructor(move: Move, optionContainer: SelectorElement) {
         this.move = move;
         this.text = MoveDirection[move?.direction!];
@@ -15,110 +17,119 @@ class Option {
         this.prepareElement(this.option);
         this.option.addEventListener('click', event => this.AddSelectedOption(event));
     }
-
+    
     GetOptionAsElement(): HTMLElement {
         return this.option;
     }
-
+    
     GetSelectedOptionAsElement(): HTMLElement {
         return this.selectedOption;
     }
-
+    
     private prepareElement(element: HTMLElement) {
         element.classList.add('option');
         element.textContent = this.text;
     }
-
+    
     private AddSelectedOption(ev: MouseEvent) {
         this.prepareElement(this.selectedOption);
         this.optionContainer.AddSelectedOption(this);
     }
-
+    
 }
 
 class ValidationElement {
     private validateContainer = document.createElement("div");
     private validateButton = document.createElement("button");
-    private level: Level;
-
-    constructor(level: Level) {
+    private parentSelectorElement: SelectorElement;
+    
+    constructor(parentSelectorElement: SelectorElement) {
         this.validateContainer.classList.add("buttonNotReady");
         this.validateButton.textContent = "Not ready";
         this.validateContainer.appendChild(this.validateButton);
-        this.level = level;
+        this.parentSelectorElement = parentSelectorElement
     }
-
+    
     GetAsElement() {
         return this.validateContainer;
     }
-
+    
     EnableValidateButton() {
         this.validateContainer.classList.add("buttonReady");
         this.validateContainer.classList.remove("buttonNotReady");
         this.validateContainer.firstChild!.textContent = "Ready";
-        this.validateContainer.addEventListener("click", event => this.GetFilledLevel(event));
+        this.validateContainer.addEventListener("click", event => this.Validate(event));
     }
-
-    GetFilledLevel(event: Event) {
+    
+    Validate(event: Event) {
+        const parentSelectorElement = this.parentSelectorElement;
         axios.get('fillLevel', {
             params: {
-                level: this.level.toJsonString(),
+                level: LEVEL.toJsonString(),
             }
         }).then(function (response) {
-            // handle success
-            const level = new Level();
-            level.fromJson(response.data);
-            console.log(level.toJson());
-            console.log(level.toJsonString());
+            LEVEL.fromJson(response.data);
+            parentSelectorElement.Validate();
         })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-            .finally(function () {
-                console.log("All good");
-            });
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function () {
+            console.log("All good");
+        });
+    }
+
+    Hide() {
+        this.validateContainer.hidden = true;
     }
 }
 
 class SelectorElement {
-    private level: Level;
+    private container: HTMLElement = document.createElement("div");
     private selector: HTMLElement = document.createElement("div");
     private selection: HTMLElement = document.createElement("div");
+    private optionsContainer: HTMLElement = document.createElement("div");
     private validateElement: ValidationElement;
-
-    constructor(level: Level) {
-        this.level = level;
+    private parentWhereIsMyDotApp: WhereIsMyDotApp;
+    
+    constructor(parentWhereIsMyDotApp: WhereIsMyDotApp) {
+        this.init();
         this.GenerateOptions();
         this.GenerateSelectorElement();
         this.GenerateSelectionElement();
-        this.validateElement = new ValidationElement(this.level);
+        this.validateElement = new ValidationElement(this);
         this.selector.appendChild(this.validateElement.GetAsElement());
+        this.parentWhereIsMyDotApp = parentWhereIsMyDotApp;
     }
-
+    
     GetAsElement(): HTMLElement {
-        return this.selector;
+        return this.container;
     }
 
+    private init() {
+        this.container.setAttribute("id", "selectorContainer");
+        this.container.classList.add("banner");
+    }
+    
     private GenerateSelectorElement() {
         this.selector.setAttribute("id", "selector");
-        this.selector.classList.add("banner");
+        this.container.appendChild(this.selector);
     }
-
+    
     private GenerateOptions() {
-        const optionsContainer = document.createElement("div");
-        for (const move of this.level.allowedMoves) {
-            console.log(move);
+        this.optionsContainer.setAttribute("id", "optionsContainer")
+        for (const move of LEVEL.allowedMoves) {
             const option = new Option(move, this);
-            optionsContainer.appendChild(option.GetOptionAsElement());
+            this.optionsContainer.appendChild(option.GetOptionAsElement());
         }
-        this.selector.appendChild(optionsContainer);
+        this.selector.appendChild(this.optionsContainer);
     }
 
     private GenerateSelectionElement() {
         this.selection.setAttribute("id", "selection");
-        this.selector.appendChild(this.selection);
-        for (var i = 0; i < this.level?.moves!; i++) {
+        this.container.appendChild(this.selection);
+        for (var i = 0; i < LEVEL.moves!; i++) {
             const emptyOption = document.createElement("span");
             emptyOption.classList.add("emptyOption");
             this.selection.appendChild(emptyOption);
@@ -129,24 +140,36 @@ class SelectorElement {
         const elements = this.selection.getElementsByClassName("emptyOption");
         const elementToReplace = elements[0]
         elementToReplace.replaceWith(option.GetSelectedOptionAsElement());
-        this.level.grid?.indigenous?.trajectory?.moves.push(option.move);
-        if (this.level.grid?.indigenous?.trajectory?.moves?.length! === this.level.moves) {
+        LEVEL.grid?.indigenous?.trajectory?.moves.push(option.move);
+        if (LEVEL.grid?.indigenous?.trajectory?.moves?.length! === LEVEL.moves) {
             this.validateElement.EnableValidateButton();
         }
+    }
+
+    // Object LEVEL has been filled in with moves and initial positions.
+    Validate() {
+        this.parentWhereIsMyDotApp.BuildGrid();
+    }
+
+    HideSelector() {
+        console.log("hidden");
+        this.selector.hidden = true;
+    }
+
+    HideValidateElement() {
+        this.validateElement.Hide();
     }
 
 }
 
 class WhereIsMyDotApp {
-    private readonly level: Level;
     private readonly container: HTMLElement = document.body;
     private readonly header: HTMLElement = document.createElement("div");
     private readonly footer: HTMLElement = document.createElement('div');
     private selectorElement: SelectorElement;
 
-    constructor(level: Level) {
-        this.level = level;
-        this.selectorElement = new SelectorElement(this.level)
+    constructor() {
+        this.selectorElement = new SelectorElement(this)
     }
 
     Init() {
@@ -172,6 +195,11 @@ class WhereIsMyDotApp {
         this.footer.textContent = "2023";
         this.container.appendChild(this.footer);
     }
+
+    BuildGrid() {
+        this.selectorElement.HideSelector();
+        this.selectorElement.HideValidateElement();
+    }
 }
 
 function handleMoveChoice(this: HTMLElement, event: Event) {
@@ -179,14 +207,13 @@ function handleMoveChoice(this: HTMLElement, event: Event) {
 }
 
 function getInitialLevel() {
-    const level = new Level();
     axios({
         method: 'get',
         url: 'getInitialLevel'
     }).then(function (response) {
         // handle success
-        level.fromJson(response.data);
-        const app = new WhereIsMyDotApp(level);
+        LEVEL.fromJson(response.data);
+        const app = new WhereIsMyDotApp();
         app.Init();
     })
         .catch(function (error) {
